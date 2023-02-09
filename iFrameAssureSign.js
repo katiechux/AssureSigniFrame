b072e7b04d5acd257ae7235479e5a4e8514f22db
katie Chu <katiechux@gmail.com>
2023-02-09 14:34:59 -0800

    test

--- iFrameAssureSign.js
+++ iFrameAssureSign.js
@@ -21,37 +21,135 @@ export class SampleIframe extends LitElement {
     static getMetaConfig() {
         // plugin contract information
         return {
-            controlName: 'IFrame-new',
+            controlName: 'AssureSign-iFrame',
             fallbackDisableSubmit: false,
-            description: 'IFrame component which can render url view with the frame',
-            iconUrl: "one-line-text",
-            groupName: 'Visual',
+            description: 'IFrame component which can render AssureSign envelope',
+            iconUrl: "pen",
+            groupName: 'signature',
             version: '1.3',
             properties: {
-                src: {
-                    type: 'string',
-                    title: 'Source URL',
-                    description: 'URL of the iframe, please note many sites block been rendered in iframes'
-                },
                 height: {
                     type: 'string',
                     title: 'Height',
                     description: 'Height of the component',
+                },
+                signerEmail: {
+                    type: 'string',
+                    title: 'Signer Email'
+                },
+                signerName: {
+                    type: 'string',
+                    title: 'Signer Name'
+                },
+                signerPhone: {
+                    type: 'string',
+                    title: 'Signer Phone Number'
+                },
+                assureSignApiUsername: {
+                    type: 'string',
+                    title: 'AssureSign API Username'
+                },
+                assureSignApiKey: {
+                    type: 'string',
+                    title: 'AssureSign API Password'
+
+                },
+                assureSignApiUserEmail: {
+                    type: 'string',
+                    title: 'AssureSign API User Email'
+                },
+                assureSignTemplateId: {
+                    type: 'string',
+                    title: 'AssureSign template Id'
                 }
             },
             standardProperties: {
                 readOnly: true,
-                required: true,
                 description: true,
             }
         };
     }
 
     static properties = {
-        name: 'Hello',
-        title: 'Hello',
+        name: '',
+        title: '',
         src: 'https://www.nintex.com/',
-        height: '100%'
+        height: '100%',
+        signerName: '',
+        signerEmail: '',
+        signerPhone: '',
+        assureSignApiUsername: '',
+        assureSignApiKey: '',
+        assureSignTemplateId: ''
+    }
+
+    async load() {
+        const apiUserBody = {
+            "request": {
+              "apiUsername": this.apiUsername,
+              "key": this.assureSignApiKey,
+              "contextUsername": this.assureSignApiUserEmail,
+              "sessionLengthInMinutes": 10
+            }
+        };
+
+        const token = await fetch('https://account.assuresign.net/api/v3.7/authentication/apiUser', {
+            method: 'POST',
+            body: JSON.stringify(apiUserBody)
+        });
+
+        console.log(token);
+
+        // const submitBody = {
+        //     "request": {
+        //         "placeholders": [],
+        //         "templates": [
+        //             {
+        //                 "templateID": this.assureSignTemplateId,
+        //                 "values": [
+        //                     {
+        //                         "name": "Envelope Name 2 ",
+        //                         "value": "My New Envelope"
+        //                     },
+        //                     {
+        //                         "name": "Language",
+        //                         "value": "en-US"
+        //                     },
+        //                     {
+        //                         "name": "Signer 1 Name",
+        //                         "value": this.signerName
+        //                         },
+        //                     {
+        //                         "name": "Signer 1 Email",
+        //                         "value": this.signerEmail
+        //                     },
+        //                     {
+        //                         "name": "Signer 1 Phone",
+        //                         "value": this.signerPhone
+        //                     }
+        //                 ]
+        //             }
+        //         ]
+        //     }
+        // }
+        
+        // const submit = await fetch('https://www.assuresign.net/api/documentnow/v3.7/submit',
+        // {
+        //     method: 'POST',
+        //     headers: {
+        //         'Authorization': 'Bearer' + token.
+        //         // 'Content-Type': 'application/x-www-form-urlencoded',
+        //     },
+        //     body: JSON.stringify(submitBody)
+        // });
+
+        // console.log(submit);
+
+        // const signingLinks = await fetch('https://www.assuresign.net/api/documentnow/v.37/envelope/'+ submit.value +'/signingLinks',
+        //     {
+        //         method: 'GET'
+        //     }
+        // );
     }
 
     // Render the UI as a function of component state
@@ -60,12 +158,10 @@ export class SampleIframe extends LitElement {
 
         return html`
             <iframe
-                    class="frame"
-                    style=${styleMap(styles)}
-                    name=${this.name}
-                    allow="geolocation *; microphone; camera"
-                    title=${this.title}
-                    src=${this.src}
+                class="frame"
+                style=${styleMap(styles)}
+                allow="geolocation *; microphone; camera"
+                src=${this.src}
             ></iframe>`;
     }
 }
